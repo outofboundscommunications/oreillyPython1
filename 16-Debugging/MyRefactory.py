732cace3f6fd989d90da2deb4aee9a2b930609de
outofboundscommunications <info@outofboundscommunications.com>
2015-08-22 17:02:44 +0000

    commit of forgotten MyRefactory file from lesson

--- 16-Debugging/MyRefactory.py
+++ 16-Debugging/MyRefactory.py
@@ -8,28 +8,22 @@ case the word is correctly capitalized.
 #assign title of book
 title = 'DIVE Into python'
 #break the title into words and convert to lower case
-print('the title we started with is: ', title)
+print('the title we start with is: ', title)
 #count the number of words in title
 lst_of_words = title.lower().split()
 print('the list of words we made with the split function is: ', lst_of_words)
-'''
-for word in lst_of_words:
-        print('now we are looping thru the words in the lst_of_words. we are at: ', word)
-        #count the number of words in title
-        num_of_words = len(lst_of_words)
-        #initialize counter
-        wordCounter = 0
-        #check to see if the word is the first word in the lst_of_words. if it is, we don't need to check if preposition or not
-        if word
-        #check to see if the word is in the preposition list
-        if word in small_words:
-            print('the word,',word, ' is a preposition.')
-        else:
-             print('the word,',word, ' is not a preposition.')
-'''
+new_title ='' 
 for i,word in enumerate(lst_of_words):
     if i==0:
+        #can ignore if preposition since first word, just go ahead and capitalize first letter
         print('this is the first word of the list of words:', word )
-        new_title = new_title + word[0].upper()
+        new_title = new_title + word[0].upper()+word[1:]
+        print(new_title)
+        return new_title
     else:
         print('this is the :',i,'th word of the list of words: ', word)
+        if word in small_words:
+            new_title = new_title + word
+        else:
+            new_title = new_title + word[0].upper()+word[1:]
+
