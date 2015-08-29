24bb9faaa41777c62c8eeb0be20c34cc74e19c44
outofboundscommunications <info@outofboundscommunications.com>
2015-08-29 19:38:52 +0000

    final working copy - fixed and ready for deployment

--- 16-Debugging/MyRefactory.py
+++ 16-Debugging/MyRefactory.py
@@ -1,8 +1,4 @@
-""" Takes a string and returns a title-case string.
-All words EXCEPT for small words are made title case
-unless the string starts with a preposition, in which
-case the word is correctly capitalized.
-"""
+
 #define list of prepositions
 small_words = ('into', 'the', 'a', 'of', 'at', 'in', 'for', 'on')
 def book_title(title):
@@ -25,32 +21,39 @@ def book_title(title):
     #create variable to hold the new (title case) title as we build it
     new_title ='' 
     for i,word in enumerate(lst_of_words):
+        #count the number of words in the title so we can use to determine if we need to add trailing space
+        #if final word in title we don't want to add trailing space
+        numWordsInTitle = len(lst_of_words) -1 
         #If index is 0, its the first word of the title, so just go ahead and capitalize first letter
-        #e.g. you don't need to check if preposition or not. doesnt matter.
         if i==0:
             new_title += word.title()+" "
-            #replace the existing word in the list with the new title case changed word
-            #lst_of_words[i] = new_title
         #check and see if the word is in the list of 'small words' or prepositions
         else:
-            #if word is preposition then skip.
+            #if word is preposition then don't need to change case, just append word as is
             if word in small_words:
-                new_title +=word+" "
-                #replace the existing word in the list with the new title case changed word
-                #lst_of_words[i] = new_title
+                #if it's the last word in title, don't add trailing space
+                if i == numWordsInTitle:
+                    new_title +=word
+                else:
+                    new_title +=word +" "
             #word is not a preposition so go ahead and make first letter title case.
             else:
-                new_title += word.title()+" "
-                #replace the existing word in the list with the new title case changed word
-                #lst_of_words[i] = new_title
-    #concatenate the new, correctly cased word to the new_title trunk
+                #if it's the last word in title, don't add trailing space
+                if i == numWordsInTitle:
+                    new_title +=word.title()
+                else:
+                    new_title +=word.title()+" "
     return new_title 
 
 def _test():
     import doctest, MyRefactory
     return doctest.testmod(MyRefactory)
-    print('test successful')
-    
+
 if __name__ == "__main__":
     _test()
+ 
 
+'''
+for title in lst_of_titles:
+    print(book_title(title))
+'''
