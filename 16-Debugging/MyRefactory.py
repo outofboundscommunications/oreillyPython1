76c5c138594e0419377c41f8569079fbcbe0d250
outofboundscommunications <info@outofboundscommunications.com>
2015-08-29 16:46:59 +0000

    working with test module now but throws error because of final trailing space

--- 16-Debugging/MyRefactory.py
+++ 16-Debugging/MyRefactory.py
@@ -1,14 +1,25 @@
-lst_of_titles =('DIVE Into python','the great gatsby','the WORKS OF AleXANDer dumas')
-
-small_words = ('into', 'the', 'a', 'of', 'at', 'in', 'for', 'on')
 """ Takes a string and returns a title-case string.
 All words EXCEPT for small words are made title case
 unless the string starts with a preposition, in which
 case the word is correctly capitalized.
 """
-
+#define list of prepositions
+small_words = ('into', 'the', 'a', 'of', 'at', 'in', 'for', 'on')
 def book_title(title):
-    print('the title passed in is: ',title)
+    """ Takes a string and returns a title-case string.
+    All words EXCEPT for small words are made title case
+    unless the string starts with a preposition, in which
+    case the word is correctly capitalized.
+
+    >>> book_title('DIVE Into python')
+    'Dive into Python'
+
+    >>> book_title('the great gatsby')
+    'The Great Gatsby'
+
+    >>> book_title('the WORKS OF AleXANDer dumas')
+    'The Works of Alexander Dumas'
+    """
     #break the title into words and convert to lower case
     lst_of_words = title.lower().split()
     #create variable to hold the new (title case) title as we build it
@@ -34,6 +45,12 @@ def book_title(title):
                 #lst_of_words[i] = new_title
     #concatenate the new, correctly cased word to the new_title trunk
     return new_title 
-for title in lst_of_titles:
-    print(book_title(title))
+
+def _test():
+    import doctest, MyRefactory
+    return doctest.testmod(MyRefactory)
+    print('test successful')
+    
+if __name__ == "__main__":
+    _test()
 
