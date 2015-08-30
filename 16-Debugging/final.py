77e3e7ee13c0597eb48502b0d8806f7dbdebe7cc
outofboundscommunications <info@outofboundscommunications.com>
2015-08-30 19:57:52 +0000

    initial commit before start obj 2

--- 16-Debugging/final.py
+++ 16-Debugging/final.py
@@ -41,3 +41,48 @@ Also, you will probably want to define a function to return the length of each w
 include punctuation characters.
 
 '''
+
+'''
+Created on Jan 1, 2015
+
+@author: jay wilner 3
+'''
+"""Count the words, lines and characters in a chunk of text."""
+
+gettysburg = """\
+Four score and seven years ago our
+fathers brought forth on this continent,
+a new nation, conceived in Liberty, and
+dedicated to the proposition that
+all men are created equal.
+furniture store hinesville ga
+furniture stores in hinesville ga
+gibson furniture
+mattress hinesville ga
+a a b c d
+
+"""
+
+lengthct = [0]*20 # a list of 20 zeroes
+print(lengthct)
+#counts characters in text
+charct = len(gettysburg)
+
+lines = gettysburg.split("\n")
+#counts lines in text
+linect = len(lines)
+
+wordct = 0
+for line in lines:
+    #split each line into words
+    words = line.split()
+    #count the number of words in the line
+    wordct += len(words)
+    for word in words:
+        #increment the counter for the word of length, len(word), by 1
+        lengthct[len(word)] += 1
+
+print("The text contains", linect, "lines,", wordct, "words, and", charct, "characters.")
+for i, ct in enumerate(lengthct):
+    if ct:
+        print("Length", i, ":", ct)
--- Chapter5-ListsTuples/paragraph_stats.py
+++ Chapter5-ListsTuples/paragraph_stats.py
@@ -20,16 +20,22 @@ a a b c d
 """
 
 lengthct = [0]*20 # a list of 20 zeroes
+print(lengthct)
+#counts characters in text
 charct = len(gettysburg)
 
 lines = gettysburg.split("\n")
+#counts lines in text
 linect = len(lines)
 
 wordct = 0
 for line in lines:
+    #split each line into words
     words = line.split()
+    #count the number of words in the line
     wordct += len(words)
     for word in words:
+        #increment the counter for the word of length, len(word), by 1
         lengthct[len(word)] += 1
 
 print("The text contains", linect, "lines,", wordct, "words, and", charct, "characters.")
