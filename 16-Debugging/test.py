edc21244da539311d57cb6445b27b3b14b87d1b9
outofboundscommunications <info@outofboundscommunications.com>
2015-10-03 16:05:19 +0000

    commit removed directory for chap16

--- 16-Debugging/test.py
+++
@@ -1,9 +0,0 @@
-__author__ = 'outofboundscommunications'
-#To change this template use Tools | Templates.
-
-s = "this Is a Simple string"
-s = "this Is a Simple string"
-slower = s.lower() # s converted to lower case <--
-supper = s.upper() # s converted to UPPER CASE <--
-stitle = s.title() # s converted to Title Case <--
-print(s, slower, supper, stitle, sep="\n")
