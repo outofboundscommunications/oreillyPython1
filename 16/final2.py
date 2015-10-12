ef352b5df8bfac972c40c2ba837fe2165f9e56bd
outofboundscommunications <info@outofboundscommunications.com>
2015-10-12 02:30:36 +0000

    working version with scaffolding but only if hard coded range values

--- 16/final2.py
+++ 16/final2.py
@@ -101,16 +101,22 @@ for wordLength in sorted((freq.keys())):
     print("{0:<15d} {1:<25d}".format(wordLength,freq[wordLength]))
 
 xMin = min(freq)
+print('xMin is: ',xMin)
+
 xMax = max(freq)
+print('xMax is: ',xMax)
 
 #figure lowest value in y axis
 yMin = min(freq.values())
+print('yMin is: ',yMin)
 
 yMax = max(freq.values())
+print('yMax is: ', yMax)
 
 myStepY = round(yMax/10)
+print('myStepY is: ',myStepY)
 
-for y in range(144,0,-10):  # stepping down from yMax to 0 by -10 increments
+for y in range(yMax,0,-10):  # stepping down from yMax to 0 by -10 increments
     print ("{:>6} | ".format(y), end="") # suppress newline after label
     for x in range(1,10):   # needs wider range for longer word lengths
         if freq.get(x,0) >= y:  # if word count > or equal to y value...
