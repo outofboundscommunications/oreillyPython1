e6fad27131b9f656ad5b8698b81acf694fcdcba7
outofboundscommunications <info@outofboundscommunications.com>
2015-10-17 03:58:01 +0000

    commit 3rd set of changes that were handed in- only remaining feature is to create x axis

--- 16/final2.py
+++ 16/final2.py
@@ -59,9 +59,20 @@ Good work so far.
 
 --------------------------------------------------------------------
 
+notes from kirby, second attempt:
 
+I'd say this is done but for the final line of x-axis values.  Most conventions around histograms
+were established by the histogram-using community long before Python or computer programming.
+Graphs tend to use round numbers for labels.  Writing a truly generic graphing program does indeed
+require the kinds of range finding you're engaged in here.  Feel free to round up to the nearest 100 and
+count down by 20s for any graph if you want to hard code a scale.
+
+-Kirby
+
+----------------------------------------------------------------------
 
 '''
+
 import sys
 
 from string import punctuation as punc 
@@ -99,8 +110,9 @@ print("{0:<15s} {1:<10s}".format('word length:', 'frequency:'))
 #print out dictionary in table format 
 for wordLength in sorted((freq.keys())):
     print("{0:<15d} {1:<25d}".format(wordLength,freq[wordLength]))
-print(freq)
+
 xMin = min(freq)
+
 xMax = max(freq)
 
 #figure lowest value in y axis
@@ -108,18 +120,16 @@ yMin = min(freq.values())
 
 yMax = max(freq.values())
 
+print("--------------------------------------") 
 myStepY = round(yMax/10)
 
-def buildRow(y):
-    myRowList = []
-    for x in range(0,xMax):
-        theCount = freq.get(x,0)
-        print(theCount)
-        if theCount >= y:
-            myRowList.append('***')
+for y in range(yMax,0,-myStepY):  # stepping down from yMax to 0 by -myStepY increments
+    print ("{:>6} | ".format(y), end="") # suppress newline after label
+    for x in range(1,xMax+1):   # iterate through the x values from 1 to xMax
+        if freq.get(x,0) >= y:  # if word count > or equal to y value...
+            column = "***"
         else:
-            myRowList.append('   ') 
-    print(myRowList)
-    
-for y in range(yMax,-10,-myStepY):
-    buildRow(y)
+            column = "   "
+        print(column, end="") # again suppress newline
+    print() # next row
+print("       --------------------------------------")          
